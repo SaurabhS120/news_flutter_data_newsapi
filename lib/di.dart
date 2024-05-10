e752ee65f36fd0457949a08957ae00208e44ed8b
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-05-10 12:31:36 +0530

    Test cases

--- lib/di.dart
+++ lib/di.dart
@@ -1,6 +1,9 @@
+import 'package:dio/dio.dart';
+import 'package:news_flutter_data_newsapi/api_service.dart';
 import 'package:news_flutter_data_newsapi/news_repo_impl.dart';
 import 'package:news_flutter_domain/NewsDI.dart';
 import 'package:news_flutter_domain/repo/news_repo.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class NewsApiDI extends NewsDI{
   final String apiKey;
@@ -8,7 +11,8 @@ class NewsApiDI extends NewsDI{
 
   @override
   NewsRepo createNewsRepo() {
-    return NewsApiRepoImpl(apiKey:apiKey);
+    ApiService apiService = ApiService(Dio()..interceptors.add(PrettyDioLogger()), baseUrl: 'https://newsapi.org/v2');
+    return NewsApiRepoImpl(apiKey: apiKey, apiService: apiService);
   }
 
 }
